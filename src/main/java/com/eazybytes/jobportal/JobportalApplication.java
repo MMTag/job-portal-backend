a2e0d90789674c5ab8de944563a1407adcb559c1
MMTag <tagaremugdha@gmail.com>
2026-07-30 00:28:16 +0530

    Integrating H2 database & getAll API

--- src/main/java/com/eazybytes/jobportal/JobportalApplication.java
+++ src/main/java/com/eazybytes/jobportal/JobportalApplication.java
@@ -1,13 +1,14 @@
-package com.eazybytes.backend;
+package com.eazybytes.jobportal;
 
+import com.eazybytes.jobportal.config.web.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class JobportalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(JobportalApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/eazybytes/jobportal/service/ICompanyService.java
@@ -0,0 +1,9 @@
+package com.eazybytes.jobportal.service;
+
+import com.eazybytes.jobportal.dto.CompanyDto;
+
+import java.util.List;
+
+public interface ICompanyService {
+    List<CompanyDto> getAllCompanies();
+}
