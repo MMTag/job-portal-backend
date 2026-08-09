4c42dd7e74b2c40a8cd14f8667f9a8323bc3748e
MMTag <tagaremugdha@gmail.com>
2026-08-09 14:13:56 +0530

    Logging simulation - 2 : using lombok @Slf4j

--- src/main/java/com/eazybytes/jobportal/company/controller/LoggingController.java
+++ src/main/java/com/eazybytes/jobportal/company/controller/LoggingController.java
@@ -1,5 +1,6 @@
 package com.eazybytes.jobportal.company.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,17 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/logging")
+@Slf4j
 public class LoggingController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingController.class);
 
     @GetMapping(path="/public",version = "1.0")
     public ResponseEntity<String> testLogging(){
-        LOGGER.trace("🔍 TRACE: This is a very detailed trace log. Used for tracking execution flow.");
-        LOGGER.debug("🐞 DEBUG: This is a debug message. Used for debugging.");
-        LOGGER.info("ℹ️ INFO: This is an informational message. Application events.");
-        LOGGER.warn("⚠️ WARN: This is a warning! Something might go wrong.");
-        LOGGER.error("🚨 ERROR: An error occurred! This needs immediate attention.");
+        log.trace("🔍 TRACE: This is a very detailed trace log. Used for tracking execution flow.");
+        log.debug("🐞 DEBUG: This is a debug message. Used for debugging.");
+        log.info("ℹ️ INFO: This is an informational message. Application events.");
+        log.warn("⚠️ WARN: This is a warning! Something might go wrong.");
+        log.error("🚨 ERROR: An error occurred! This needs immediate attention.");
         return ResponseEntity.status(HttpStatus.OK)
                 .body("Logging Tested Successfully");
     }
