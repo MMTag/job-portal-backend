94e4a8b2a528a3bc2d1f9575354b8ce079c1712b
MMTag <tagaremugdha@gmail.com>
2026-08-13 22:30:08 +0530

    Authorization in springboot app

--- src/main/java/com/eazybytes/jobportal/contact/contoller/ContactController.java
+++ src/main/java/com/eazybytes/jobportal/contact/contoller/ContactController.java
@@ -3,6 +3,7 @@ package com.eazybytes.jobportal.contact.contoller;
 import com.eazybytes.jobportal.contact.service.IContactService;
 import com.eazybytes.jobportal.contact.service.impl.ContactServiceImpl;
 import com.eazybytes.jobportal.dto.ContactRequestDto;
+import com.eazybytes.jobportal.dto.ContactResponseDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -12,6 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/contacts")
@@ -37,5 +40,11 @@ public class ContactController {
                                              @Validated @NotBlank(message = "Status cannot be blank")
                                              @Size(min=4,message = "Status length should be minimum 4 chars") String status){
         return ResponseEntity.ok("These are contacts with given status: "+ status);
+    }
+
+    @GetMapping("/admin")
+    public ResponseEntity<List<ContactResponseDto>> fetchNewContactMsgs() {
+        List<ContactResponseDto> contactResponseDtos = contactService.fetchNewContactMsgs();
+        return ResponseEntity.status(HttpStatus.OK).body(contactResponseDtos);
     }
 }
--- src/main/java/com/eazybytes/jobportal/repository/ContactRepository.java
+++ src/main/java/com/eazybytes/jobportal/repository/ContactRepository.java
@@ -2,9 +2,21 @@ package com.eazybytes.jobportal.repository;
 
 import com.eazybytes.jobportal.entity.Company;
 import com.eazybytes.jobportal.entity.Contact;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContactRepository extends JpaRepository<Contact,Long> {
+    List<Contact> findContactsByStatus(String status);
+
+    List<Contact> findContactsByStatusOrderByCreatedAtAsc(String status);
+
+    List<Contact> findContactsByStatus(String status, Sort sort);
+
+    Page<Contact> findContactsByStatus(String status, Pageable pageable);
 }
