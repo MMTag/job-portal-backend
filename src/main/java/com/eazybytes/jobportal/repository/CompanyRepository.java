0487517da58849eb31f5a820a7b9f0661cb69fb4
MMTag <tagaremugdha@gmail.com>
2026-08-19 19:13:19 +0530

    Caching using caffeine cache

--- src/main/java/com/eazybytes/jobportal/repository/CompanyRepository.java
+++ src/main/java/com/eazybytes/jobportal/repository/CompanyRepository.java
@@ -2,6 +2,7 @@ package com.eazybytes.jobportal.repository;
 
 import com.eazybytes.jobportal.entity.Company;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +15,7 @@ import java.util.List;
 @Repository
 public interface CompanyRepository extends JpaRepository<Company,Long> {
 
+    @Cacheable("jobs")
     @Query("SELECT DISTINCT c FROM Company c JOIN FETCH c.jobs j WHERE j.status = :status")
     List<Company> findAllWithJobsByStatus(@Param("status") String status);
 
